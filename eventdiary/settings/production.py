a0d7f387ff36293b9246d83930b2d55377f48af6
andela-gokerayi <okerayigbolahan@yahoo.com>
2016-12-27 01:46:40 +0100

    Setup prod db

--- eventdiary/settings/production.py
+++ eventdiary/settings/production.py
@@ -1,25 +1,11 @@
 from .base import *
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-# Parse database configuration from $DATABASE_URL
 import dj_database_url
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 ALLOWED_HOSTS = ['theeventdiary.herokuapp.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'theeventdiary',
-        'USER': 'theeventdiary',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-# DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config()}
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False 
 
