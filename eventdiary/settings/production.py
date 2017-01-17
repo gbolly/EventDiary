ce9113299ba82d8871678e27f333dc9d2609873d
andela-gokerayi <okerayigbolahan@yahoo.com>
2017-01-17 16:53:56 +0100

    Change debug to true

--- eventdiary/settings/production.py
+++ eventdiary/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['theeventdiary.herokuapp.com']
 
 DATABASES = {'default': dj_database_url.config()}
