a73f378bc4a22596706cdfc79a60815fdc3a29cc
andela-gokerayi <okerayigbolahan@yahoo.com>
2017-01-18 08:40:58 +0100

    Update auth styling

--- eventdiary/settings/base.py
+++ eventdiary/settings/base.py
@@ -123,22 +123,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# ANYMAIL = {
-#     "MAILGUN_API_KEY": envvars.get('ACCESS_KEY'),
-#     "MAILGUN_SENDER_DOMAIN": envvars.get('SENDER_DOMAIN'),
-#     "MAILGUN_PUBLIC_KEY": envvars.get('MAILGUN_PUB_KEY'),
-#     "MAILGUN_SMTP_LOGIN": envvars.get('MAILGUN_SMTP_LOGIN'),
-#     "MAILGUN_SMTP_PASSWORD": envvars.get('MAILGUN_SMTP_PASSWORD'),
-#     "MAILGUN_SMTP_PORT": 587,
-#     "MAILGUN_SMTP_SERVER": envvars.get('MAILGUN_SMTP_SERVER'),
-#     "MAILGUN_SEND_DEFAULTS":{
-#         "esp_extra": {"sender_domain": "mail.theeventdiary.com"}
-#     }
-# }
-
-# EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
-# DEFAULT_FROM_EMAIL = "info@theeventdiary.com"
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = envvars.get('EMAIL_HOST')
 EMAIL_HOST_USER = envvars.get('EMAIL_HOST_USER')
