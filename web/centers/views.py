984b799a2b7f05d3830677a173c50ba7a7b93173
andela-gokerayi <okerayigbolahan@yahoo.com>
2017-02-05 23:13:44 +0100

    fix end date bug and sent mails to user when booking is done

--- web/centers/views.py
+++ web/centers/views.py
@@ -4,17 +4,22 @@ from cloudinary.forms import cl_init_js_callbacks
 from django.contrib.auth.decorators import login_required
 from django.contrib.sessions.models import Session
 from django.core.urlresolvers import reverse
+from django.core.mail import EmailMultiAlternatives
+from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404, render_to_response, redirect
-from django.template import RequestContext, Engine
+from django.template import RequestContext, Engine, loader
 from django.utils import timezone
-from django.contrib import messages
 from django.views.generic.list import ListView
 from django.forms import modelformset_factory
 
 from .forms import BookingForm, CenterForm, ImageForm
 from .models import Center, Booking, State, LocalGovArea, CenterPhoto
 from context_processor import Image_Effects
+
+
+EMAIL_SENDER = 'info@theeventdiary.com'
+
 
 class CenterListView(ListView):
     model = Center
@@ -61,11 +66,13 @@ def center_detail(request, slug):
     })
 
 def booking_view(request, slug, model_class=Center, form_class=BookingForm, template_name='centers/booking_form.html'):
+
     cls_default_msgs = {
         'not_signed_in': 'You must be signed in to book this event center',
     }
     center = get_object_or_404(model_class, slug=slug)
     args = dict()
+
     if request.POST:
         form = form_class(request.user, center, request.POST)
         if request.user.is_authenticated():
@@ -74,9 +81,43 @@ def booking_view(request, slug, model_class=Center, form_class=BookingForm, temp
                 booking.center = center
                 booking.user_id = request.user.id
                 booking.is_approved = False
+
+                start_date = form.cleaned_data.get("booking_start_date")
+                end_date = form.cleaned_data.get("booking_end_date")
+
+                if end_date < start_date:
+                    msg = u"The start date cannot be later than the end date. Please input an end date after the selected start date."
+                    messages.add_message(request, messages.INFO, msg, form.errors)
+                    template = Engine.get_default().get_template(template_name)
+                    context = RequestContext(request)
+                    return HttpResponse(template.render(context))
+
                 booking.save()
                 args["slug"] = slug
                 args["center"] = center.name
+
+                # compose the email
+                booking_email_context = RequestContext(
+                    request,
+                    {'username': booking.customer_name,
+                     'center': center.name,
+                     'booking': booking.booking_start_date,
+                    },
+                )
+
+                receipient = str(booking.customer_email)
+
+                subject, from_email, to = 'TheEventDiary: Booking Recieved', EMAIL_SENDER, receipient
+                html_content=loader.get_template('centers/booking_email.html').render(booking_email_context)
+                text_content=loader.get_template('centers/booking_email.txt').render(booking_email_context)
+
+                msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+                msg.attach_alternative(html_content, "text/html")
+                response = msg.send()
+
+                if response == 1:
+                    messages.add_message(request, messages.INFO, booking.customer_email)
+
                 return render(request, 'thank_you.html', {"center":center.name})
             else :
                 return render(request, template_name, {'form': form})
